618a58ee35aefec464f5086803f59f28b5b20e35
Arif Akram Mohammed <m.arifakram@gmail.com>
2017-04-02 16:50:40 -0500

    authorize a user for registration

--- templates/admin/authorize.js
+++ templates/admin/authorize.js
@@ -10,13 +10,67 @@ angular.module('computingServices.authorize', ['ngRoute'])
     })
 }])
 
-.controller('AuthorizeCtrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+.factory('AuthorizeService', ['$http', '$q', function ($http, $q) {
 
-    $scope.authorize = function (id) {
-        if(id !== undefined) {
-            console.log('authorizing user ',id);
-            notifyUser('User with id '+id+' can now register.');
-            $scope.clearForm();
+    var AUTHORIZE_USER_URI = constants.url + 'admin/authorize';
+
+    var factory = {
+        authorize: authorize
+    };
+
+    return factory;
+
+    // AUTHORIZE A USER FOR REGISTRATION
+    function authorize(user) {
+        var deferred = $q.defer();
+
+        $http({
+                method: 'POST',
+                url: AUTHORIZE_USER_URI,
+                headers: {
+                    'Content-Type': undefined
+                },
+                transformRequest: function (data) {
+                    var formData = new FormData();
+                    formData.append("user", angular.toJson(user));
+                    return formData;
+                }
+            })
+            .success(function (data, status, headers, config) {
+                console.log('Authorize a user operation success');
+                deferred.resolve(data);
+            })
+            .error(function (data, status, headers, config) {
+                console.log('Authorizing a user failed ', status);
+                deferred.reject(data);
+            });
+        return deferred.promise;
+    }
+}])
+
+.controller('AuthorizeCtrl', ['$scope', 'AuthorizeService', 'SharedService', function ($scope, AuthorizeService, SharedService) {
+
+    $scope.authorize = function (user) {
+        if (user.userId !== undefined) {
+            user.firstName = "Arif";
+            user.lastName = "Akram";
+            user.role = "Admin";
+            console.log('authorizing user ', user);
+            //call service method to authorize a user for registration to this system
+            var promise = AuthorizeService.authorize(user);
+            promise.then(function (result) {
+                    if (result.statusCode === 200) {
+                        SharedService.showSuccess(result.message);
+                        $scope.clearForm();
+                    } else {
+                        SharedService.showError(result.message);
+                    }
+                })
+                .catch(function (resError) {
+                    console.log('AUTHORIZE FAILURE :: ', resError);
+                    //show failure message to the user
+                    SharedService.showError('Failed to authorize the user for registration');
+                });
         }
     }
 
@@ -27,13 +81,4 @@ angular.module('computingServices.authorize', ['ngRoute'])
         $scope.authorizeAUserForm.$setUntouched();
     }
 
-    //alerts to user
-    function notifyUser(message) {
-        $mdDialog.show(
-            $mdDialog.alert()
-            .clickOutsideToClose(true)
-            .textContent(message)
-            .ok('Got it!')
-        );
-    }
 }]);
