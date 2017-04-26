9fe911218ec1132fc26a4a0724c4e91cce4aaae9
Arif Akram Mohammed <m.arifakram@gmail.com>
2017-04-25 23:58:04 -0500

    Styling careers page

--- templates/general/careers/careers.js
+++ templates/general/careers/careers.js
@@ -9,15 +9,26 @@ angular.module('computingServices.careers', ['ngRoute'])
     });
 }])
 
-.service('CareersService', ['$http', function ($http) {
-    this.post = function (uploadUrl, submittedData, submittedFile) {
+.factory('CareersService', ['$http', '$q', function ($http, $q) {
+
+    var APPLY_JOB_URI = constants.url + 'general/careers';
+
+    var factory = {
+        applyJob: applyJob
+    };
+
+    return factory;
+
+    function applyJob(submittedData, submittedFile) {
 
         console.log('Data to be sent to the server (Raw): ', submittedData);
         console.log('Just the file data: ', submittedFile);
 
+        var deferred = $q.defer();
+
         $http({
             method: 'POST',
-            url: uploadUrl,
+            url: APPLY_JOB_URI,
             headers: {
                 'Content-Type': undefined
             },
@@ -37,64 +48,74 @@ angular.module('computingServices.careers', ['ngRoute'])
             }
         }).
         success(function (data, status, headers, config) {
-            alert("success!");
+            console.log('Apply Job Success ', status);
+            deferred.resolve(data);
         }).
         error(function (data, status, headers, config) {
-            alert("failed!");
+            console.log('Apply Job Failure ', status);
+            deferred.reject(data);
         });
 
+        return deferred.promise;
     }
 }])
 
-.controller('CareersCtrl', ['$scope', 'CareersService', '$filter', '$mdDialog', function ($scope, CareersService, $filter, $mdDialog) {
+.controller('CareersCtrl', ['$scope', 'CareersService', '$filter', '$mdDialog', 'SharedService', function ($scope, CareersService, $filter, $mdDialog, SharedService) {
 
     $scope.user = {};
 
-    $scope.files = [];
-    //listen for the file selected event which is raised from directive
-    $scope.$on("seletedFile", function (event, args) {
-        $scope.$apply(function () {
-            //add the file object to the scope's files collection
-            console.log('pushing files: ', args);
-            $scope.files.push(args.file);
-        });
-    });
-
     $scope.save = function () {
-        $scope.$broadcast('show-errors-check-validity');
 
-        if ($scope.userForm.$valid) {
-            console.log($scope.user);
-            // get the file extension
-            var allowedExtns = ['pdf', 'doc', 'docx', 'odt'];
-            if ($scope.files.length > 0) {
-                var fileExtn = $scope.files.name.split('.');
-                if (fileExtn.length === 1 || (fileExtn[0] === "" && a.length === 2) || fileExtn.length > 2 || allowedExtns.indexOf(fileExtn[1]) === -1) {
-                    notifyUser('Only the files with extensions .pdf .doc .docx .odt are permitted for upload. Please upload a valid file');
-                    return;
-                }
-                console.log('resume extn ', fileExtn[1]);
-                $scope.user.resumeExtn = fileExtn[1];
-            }
-
-            $scope.user.dateApplied = $filter('date')(new Date(), 'mediumDate');
-            /*http://127.0.0.1:8080/ComputingServicesApp/home/careers2*/
-            CareersService.post(constants.url + 'general/careers', $scope.user, $scope.files);
-            //reset form after receiving response
-            $scope.reset();
+        console.log('applicant data : ', $scope.user);
+        console.log('file data: ', $scope.files_resume);
+        console.log('file name: ', $scope.files_resume.name);
+        // permitted file extensions
+        var allowedExtns = ['pdf', 'doc', 'docx', 'odt'];
+        var fileExtn = $scope.files_resume.name.split('.');
+        if (fileExtn.length === 1 || (fileExtn[0] === "" && a.length === 2) || fileExtn.length > 2 || allowedExtns.indexOf(fileExtn[1]) === -1) {
+            notifyUser('Only the files with extensions .pdf .doc .docx .odt are permitted for upload. Please upload a valid file');
+            return;
         }
+        console.log('resume extn ', fileExtn[1]);
+        $scope.user.resumeExtn = fileExtn[1];
+
+        $scope.user.dateApplied = $filter('date')(new Date(), 'mediumDate');
+
+        var promise = CareersService.applyJob($scope.user, $scope.files_resume);
+        promise.then(function (result) {
+                //CareersService.post(constants.url + 'general/careers', $scope.user, $scope.files_resume);
+
+                if (result.statusCode === 200) {
+                    SharedService.showSuccess(result.message);
+                } else {
+                    SharedService.showError(result.message);
+                }
+            })
+            .catch(function (resError) {
+                console.log('FAILURE :: ', resError);
+                //show failure message to the user
+                SharedService.showError('Error Ocurred while submitting your profile');
+                notifyUser('Some error occurred while submitting your profile. Please try again, if you see the error again, please submit the paper based application');
+            });
+
+        //reset form after receiving response
+        $scope.reset();
     };
 
-    $scope.change = function (fileName) {
-        console.log('file being uploaded is ', fileName);
-        angular.element('#uploadFile').val(fileName);
-    };
+    //can form be submitted
+    $scope.canSubmitForm = function () {
+        if ($scope.userForm.$invalid || $scope.files_resume === undefined) {
+            return true;
+        }
+        return false;
+    }
 
+    //clear form
     $scope.reset = function () {
-        $scope.$broadcast('show-errors-reset');
         $scope.user = {};
-        angular.element('#uploadBtn').val('');
-        angular.element('#uploadFile').val('');
+        $scope.files_resume = undefined;
+        $scope.userForm.$setPristine();
+        $scope.userForm.$setUntouched();
     };
 
     //alerts to user
@@ -106,5 +127,22 @@ angular.module('computingServices.careers', ['ngRoute'])
             .ok('Got it!')
         );
     }
+
+}])
+
+.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
 
 }]);
